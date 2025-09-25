fc281807c2c3900dad29afb2ca22f7d8c47e8b60
Matheus Osses de Lima <maths0ss3s@gmail.com>
2025-09-25 11:44:21 -0300

    primeiro commit | feat: crud

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,6 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
 import router from '@adonisjs/core/services/router'
 import './routes/mensalidadeRoutes.js'
 import './routes/usuarioRoutes.js'
